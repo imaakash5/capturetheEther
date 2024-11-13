f1c7d828cbc3177233ee4303de77f4e59911b0c7
imaakash5 <44193567+imaakash5@users.noreply.github.com>
2024-11-13 17:57:19 +0530

    CTF deploy

---
+++ src/Deploy.sol
@@ -0,0 +1,10 @@
+//SPDX - License - Identifier : Unlicensed
+pragma solidity ^0.8.23;
+
+contract Deploycontract{
+
+    function deploy()public pure returns(bool){
+        return true;
+        }   
+
+}
